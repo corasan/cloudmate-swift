2ac9fd87d6935fa4aaf2db7eb2920da49d3fcee1
Henry Paulino <henrypl360@gmail.com>
2020-08-01 19:56:46 -0400

    first commit

---
+++ CloudMate/CloudMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  CloudMateApp.swift
+//  CloudMate
+//
+//  Created by Henry Paulino on 8/1/20.
+//
+
+import SwiftUI
+
+@main
+struct CloudMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
